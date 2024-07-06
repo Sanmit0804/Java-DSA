274f5149494564776f017bf0a7d4f31b25ebfef9
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-07-06 16:07:39 +0530

    prefix sum

--- Array/code7MaxSubArray.java
+++ Array/code7MaxSubArray.java
@@ -1,8 +1,36 @@
 // Max of Subarrays in optimized prefix manner
+// time complexity = O(n^2)
 
 public class code7MaxSubArray {
-    public static void main(String[] args) {
-        int numbers[] = { 1, -2, 6, -1, 3 };
+    public static void printSubArrays(int numbers[]) {
+        int maxium = Integer.MIN_VALUE; // Assign minimum value possible for integer
+        int prefix[] = new int[numbers.length];
 
+        prefix[0] = numbers[0];
+        // calculate prefix array
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i + 1; j < numbers.length; j++) {
+                int end = i;
+                int currSum;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                // System.out.println();
+                if (maxium < currSum) {
+                    maxium = currSum;
+                }
+            }
+            // System.out.println();
+        }
+        System.out.println("Highest value is : " + maxium);
+    }
+
+    public static void main(String[] args) {
+        // int numbers[] = { 1, -2, 6, -1, 3 };
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        printSubArrays(numbers);
     }
 }
