0e16bd78dc893f88c39778aecdbe0f7b55109fb7
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-07-07 12:49:19 +0530

    Array basics complete

--- Array/code10BuyAndSellStock.java
+++ Array/code10BuyAndSellStock.java
@@ -1,6 +1,22 @@
-/**
- * code10BuyAndSellStock
- */
 public class code10BuyAndSellStock {
-    
+    public static int buyAndSellStock(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) // profit
+            {
+                int profit = prices[i] - buyPrice; // today's price
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buyAndSellStock(prices));
+    }
 }
