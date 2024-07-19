092b5208de987be810e71223f70d97f97ef857ca
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-07-19 10:37:09 +0530

    Started OOP

---
+++ OOP/myClass.java
@@ -0,0 +1,23 @@
+public class myClass {
+    public static void main(String[] args) {
+        // rollno of 5 students
+        // int[] rno = new int[5];
+
+        // names of 5 students
+        // String[] names = new String[5];
+
+        Student s1 = new Student();
+        s1.rno[0] = 44;
+        s1.name[0] = "Sanmit";
+        s1.marks[0] = 87.4f;
+
+        System.out.println(s1.marks[0]);
+    }
+}
+
+class Student {
+    // (Rollno, name & marsk) of the 5 students
+    int[] rno = new int[5];
+    String[] name = new String[5];
+    float[] marks = new float[5];
+}
