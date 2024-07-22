cde6751d2d3009a317f8eb761cdf41738bd440c9
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-07-22 16:43:10 +0530

    Learn about classes and Objects

--- OOP/myClass.java
+++ OOP/myClass.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class myClass {
     public static void main(String[] args) {
         // rollno of 5 students
@@ -7,17 +9,19 @@ public class myClass {
         // String[] names = new String[5];
 
         Student s1 = new Student(); // Creating object
+        Student[] students = new Student[5];
         s1.no = 1;
         s1.name = "Sanmit";
         s1.marks = 99.99f;
 
         System.out.println(s1.marks);
+        System.out.println(Arrays.toString(students));
     }
 }
 
 // Student class
+// for every single student
 class Student {
-    // (Rollno, name & marks) of the 5 students
     int no;
     String name;
     float marks;
