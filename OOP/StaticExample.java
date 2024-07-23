3518bcf753100d7f767374367d3218d0b6a73422
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-07-23 18:11:50 +0530

    Static in java

---
+++ OOP/StaticExample.java
@@ -0,0 +1,14 @@
+public class StaticExample {
+    public static void main(String[] args) {
+        StaticExample obj = new StaticExample();
+        obj.greeting();
+    }
+
+    // void callgreeting() {
+    // greeting();
+    // }
+
+    void greeting() {
+        System.out.println("Hello Sanmit!");
+    }
+}
