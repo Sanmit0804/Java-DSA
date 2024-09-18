c7b8bbe408bbf0b522ad1683a4572b22234dcf10
Sanmit0804 <sanmitsuthar3@gmail.com>
2024-09-18 11:19:36 +0530

    Revise 2D arrays in java

--- Arrays 2D/Matrics.java
+++ Arrays 2D/Matrics.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 public class Matrics {
+
     public static boolean search(int matrix[][], int key) {
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == key) {
-                    System.out.println("Found " + matrix[i][j] + " at cell (" + i + "," + j + ")");
+                    System.out.print("Found at cell (" + i + "," + j + ")");
                     return true;
                 }
             }
@@ -13,34 +14,31 @@ public class Matrics {
         System.out.print("Key not found");
         return false;
     }
-    public static void checkLimits(int matrix[][]) {
-        int largest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
 
-        for(int i = 0; i<matrix.length; i++)
+    public static void largest(int matrix[][])
+    {
+        int largest = Integer.MIN_VALUE;
+        for(int i =0; i<matrix.length;i++)
         {
-            for(int j = 0 ; j< matrix.length ; j++)
+            for(int j = 0; j<matrix[0].length ; j++)
             {
-                if(largest<matrix[i][j])
+                if(matrix[i][j] > largest)
                 {
                     largest = matrix[i][j];
                 }
-                if(smallest > matrix[i][j])
-                {
-                    smallest = matrix[i][j];
-                }
             }
         }
-        System.out.println("Largest element in matrix is : "+ largest);
-        System.out.println("Smallest element in matrix is : "+ smallest);
+        System.out.println("Largest value in matrix is : "+ largest);
     }
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello");
-        int matrix[][] = new int[3][3];
-        int n = 3;
-        int m = 3;
+        System.out.println("Welcome to 2D array");
 
+        int matrix[][] = new int[3][3];
+        int n = matrix.length; // length of row
+        int m = matrix[0].length; // lenfth of columns
+
+        // INPUT
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -48,15 +46,18 @@ public class Matrics {
             }
         }
 
-        // output
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        // OUTPUT
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
 
-        search(matrix, 5);
-        checkLimits(matrix);
+        // Search in 2D array
+        search(matrix, 9);
+
+        // Largest in 2D array
+        largest(matrix);
     }
 }
